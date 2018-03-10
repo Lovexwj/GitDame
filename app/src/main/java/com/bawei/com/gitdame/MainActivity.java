f48c72b6d8345b00175bc636c377efec86ed739b
Lovexwj <1911901753@qq.com>
2018-03-10 10:21:44 +0800

    dsd

--- app/src/main/java/com/bawei/com/gitdame/MainActivity.java
+++ app/src/main/java/com/bawei/com/gitdame/MainActivity.java
@@ -10,8 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
            //张露
-           //小胡
-           //双薪
-           //大哥
+
     }
 }
